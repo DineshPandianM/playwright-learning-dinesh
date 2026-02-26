f1585fa78d6764d4f198a734809a87a95682d375
Dinesh Pandian <dineshpandianm98@gmail.com>
2026-02-26 21:23:00 +0530

    Day 02 - Loan eligibility program

---
+++ Day01/day02conditional.js
@@ -0,0 +1,15 @@
+let age = 19
+let salary =4000
+let credit = 700
+
+if(age<18)
+{
+    console.log("Not Eligible for loan")
+}
+else if (salary>=35000 && credit>=750)
+{
+    console.log("Eligible and Got approval for loan")
+}
+else {
+    console.log("Loan Rejected")
+}
