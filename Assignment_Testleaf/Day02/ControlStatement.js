fda461d4dc7fd900965de87eedc42bd2c7f1e33c
Dinesh Pandian <dineshpandianm98@gmail.com>
2026-03-01 18:47:04 +0530

    Added Assignment Testleaf Day01 and Day02

---
+++ Assignment_Testleaf/Day02/ControlStatement.js
@@ -0,0 +1,6 @@
+
+for(i=1;i<=20;i++){
+    if(i%2!=0){
+        console.log("These all are odd numbers 1 to 20 is : " + i)
+    }
+}
---
+++ Assignment_Testleaf/primativedatatypes.js
@@ -0,0 +1,9 @@
+var CompanyName = "Bright Solution"
+var mobileNumber = 8899774455
+var isAutomation = true
+var hasPlaywright 
+
+console.log(typeof CompanyName)
+console.log(typeof mobileNumber)
+console.log(typeof isautomation)
+console.log(typeof hasplaywright)
